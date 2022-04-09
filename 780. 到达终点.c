69f6e946e797da9f9c27c1f55a32fd8654bcc11b
XiaoJu <1600549245@qq.com>
2022-04-09 20:20:34 +0800

    first

---
+++ 780. 到达终点.c
@@ -0,0 +1,18 @@
+bool reachingPoints(int sx, int sy, int tx, int ty){
+  while (tx > sx && ty > sy) {
+      if (tx > ty) {
+          tx %= ty;
+      } else {
+          ty %= tx;
+      }
+  }
+  if (tx == sx && ty == sy) {
+      return true;
+  } else if (tx == sx) {
+      return ty > sy && (ty - sy) % tx == 0;
+  } else if (ty == sy) {
+      return tx > sx && (tx - sx) % ty == 0;
+  } else {
+      return false;
+  }
+}
